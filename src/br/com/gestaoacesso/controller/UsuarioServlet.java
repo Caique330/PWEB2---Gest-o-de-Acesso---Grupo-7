8009dfb6bc335830e2cd4dd3ba649a0dfb57d45c
Caíque Rocha <cers3@aluno.ifal.edu.br>
2025-10-14 08:22:09 -0300

    Update UsuarioServlet.java

--- src/br/com/gestaoacesso/controller/UsuarioServlet.java
+++ src/br/com/gestaoacesso/controller/UsuarioServlet.java
@@ -1,19 +1,26 @@
 package br.com.gestaoacesso.controller;
 
 import java.io.IOException;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet("/UsuarioServlet")
 public class UsuarioServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 1L;
+       
+    
     public UsuarioServlet() {
         super();
+      
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.getWriter().append("Servlet funcionando! Projeto Gestão de Acesso");
-    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Servlet funcionando! Projeto Gestão de Acesso");
+	}
+    
 }
